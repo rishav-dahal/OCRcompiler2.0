f4c3f326754537fa857351d51a29a1efc0cae902
Dhiraj <dhrjpoudyal@gmail.com>
2024-07-27 00:14:56 +0545

    landing page updated

--- Client/src/pages/Landingpage.jsx
+++ Client/src/pages/Landingpage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "../App.css";
 import "./Landingpage.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import homepagess from "../images/homepagess.png";
+import wigglevector from "../images/wigglevector.svg";
+import { TbScan } from "react-icons/tb";
+import { TbCapture } from "react-icons/tb";
+import { FaCode } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Landingpage = () => {
   const navigate = useNavigate();
 
   function handleClick() {
-    navigate('/login');
+    navigate("/login");
   }
   return (
     <>
@@ -24,7 +30,50 @@ const Landingpage = () => {
       </div>
       <div className="container">
         <div className="hero-section">
-          Scan and compile code snippets in instant
+          Scan and compile <br />{" "}
+          <span className="wiggle-wrapper">
+            code snippets
+            <img src={wigglevector} className="wiggle-svg" width={450}></img>
+          </span>{" "}
+          in an instant.
+        </div>
+        <div className="landing-img">
+          <img src={homepagess} alt="landing_img" width={1000}></img>
+        </div>
+        <div className="features-wrapper">
+          <div className="feature">
+            <div className="feature-icon">
+              <TbCapture />
+            </div>
+            <div className="feature-description">
+              Capture a code <br />
+              snippet.
+            </div>
+          </div>
+          <div className="right-arrow-icon">
+            <FaArrowRight />
+          </div>
+          <div className="feature">
+            <div className="feature-icon">
+              <TbScan />
+            </div>
+            <div className="feature-description">
+              Extract the code <br />
+              into digital format.
+            </div>
+          </div>
+          <div className="right-arrow-icon">
+            <FaArrowRight />
+          </div>
+          <div className="feature">
+            <div className="feature-icon">
+              <FaCode />
+            </div>
+            <div className="feature-description">
+              Compile the <br />
+              code.
+            </div>
+          </div>
         </div>
       </div>
     </>
